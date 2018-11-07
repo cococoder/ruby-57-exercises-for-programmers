82710a25ef74f2bd0d109ed043e47334dcae1389
The Coco Coder <shout@cococoder.com>
2018-11-07 21:58:11 +0000

    Add a failing cli test
    This change create a complete end to end test showing a full example if
    it the application is working correctly.
    
    As you can see there is a concept of a somesthing called runtime. I am
    a big believer in only testing my own code, as this example builds out
    you will see that abstraction will make the code more testable.

--- say-hello/lib/say_hello.rb
+++ say-hello/lib/say_hello.rb
@@ -1,7 +1,20 @@
 #Objective: “Create a program that prompts for your name and prints a greeting using your name.”
 #Constraint: “Keep the input, string concatenation, and output separate”
 
+#Psuedo code
 
+#question = Question.new body: "What is your name?", ui: self
+#answer = question.ask?
+#greeting = Greeting.new name: answer, ui: self
+#greeting.display
 
+module SayHello
+  class Cli
+    def initialize runtime:
 
+    end
+    def run
 
+    end
+  end
+end
---
+++ tests/cli_test.rb
@@ -0,0 +1,24 @@
+require 'test/unit'
+require "test/unit/rr"
+require_relative '../say-hello/lib/say_hello'
+
+class CliTest < Test::Unit::TestCase
+  def test_run
+    #Given
+    name = "Brian"
+    runtime = mock!
+
+    #expect
+    mock(runtime).puts("What is your name?")
+    mock(runtime).gets {"Brian"}
+    mock(runtime).puts("Hello, Brian, nice to meet you!")
+    @cli = SayHello::Cli.new runtime: runtime
+
+    #when
+    @cli.run
+
+  end
+end
+
+
+
